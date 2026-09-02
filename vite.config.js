5edf0e5b74aa4268bcb2a4d27f6099ad5bb202e8
Abdul Wasay <hafizabdulwasay771@gmail.com>
2026-09-02 20:06:56 +0500

    Load responsive CSS automatically during Vite build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/abdulwasay-portfolio/',
+  plugins: [
+    {
+      name: 'inject-mobile-responsive-css',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          '  <link rel="stylesheet" href="/abdulwasay-portfolio/mobile-fix.css">\n  </head>'
+        );
+      },
+    },
+  ],
 });
